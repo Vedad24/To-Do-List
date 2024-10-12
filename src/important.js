2d95e4326799e2a0ba416c8ed2901c3cb524961b
vedad <vedolino2004@gmail.com>
2024-10-12 23:07:46 +0200

    add feature to have taks with important priority to go to important section

--- src/important.js
+++ src/important.js
@@ -3,5 +3,34 @@ import { refreshTasks } from "./dialog";
 
 export function important() {
     contentDOM.sectionName.innerHTML = "Important";
-    refreshTasks(contentDOM.sectionName.innerHTML);
+    contentDOM.tasks.innerHTML = "";
+    contentDOM.allItemTasks.forEach(task => {
+        if (task.priority != true) {
+            return;
+        }
+        contentDOM.tasks.innerHTML +=
+        `
+            <div class="task">
+                <div class="taskText">
+                    <input type="checkbox" class="mark">
+                    <p>${task.title}</p>
+                </div>
+                <div class="taskButtons">
+                    <button class="edit"><i class="fa-solid fa-pen fa-lg" style="color: #000000;"></i></button>
+                    <button class="delete"><i class="fa-solid fa-trash fa-lg" style="color: #000000;"></i></button>
+                </div>
+            </div>
+        `
+    });
+
+    document.querySelectorAll(".mark").forEach(mark => mark.addEventListener("click", function () {
+        if (mark.checked == true) {
+            mark.nextElementSibling.style.textDecoration = "line-through";
+            mark.nextElementSibling.style.color = "gray";
+        } 
+        else {
+            mark.nextElementSibling.style.textDecoration = "none";
+            mark.nextElementSibling.style.color = "black";
+        }
+    }))
 }
